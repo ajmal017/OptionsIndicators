c98d056747ee2d9728ef2d6671ff887305623b89
nvshah286 <nvshah286@gmail.com>
2019-03-30 20:18:40 -0500

    webscrap the data from URL as a data frame for options
    webscrap the data from URL as a data frame for options

--- importDataYahoo.py
+++ importDataYahoo.py
@@ -2,6 +2,7 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
 
 optionsURL = 'https://finance.yahoo.com/quote/TSLA/options?p=TSLA'
 optionsPage = urlopen(optionsURL)
@@ -9,3 +10,15 @@ htmldata = BeautifulSoup(optionsPage)
 
 ## trying to fetch the html data
 temp = htmldata.find("table")
+
+headings = [th.get_text() for th in temp.find("tr").find_all("th")]
+
+datasets = pd.DataFrame()
+for row in temp.find_all("tr")[1:]:
+    dataset =pd.Series(td.get_text() for td in row.find_all("td"))
+    print(dataset)
+    datasets = datasets.append(dataset,ignore_index= True)
+
+datasets.columns = ['ContractName','LastTradeDate','Strike','LastPrice','Bid','Ask','Change','PctChange',
+                   'Volume'	,'OpenInterest','ImpliedVolatility']
+
