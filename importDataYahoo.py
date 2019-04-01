ae8491db9c9dff335ec4839493e16b1629ea9ee3
Nirav Shah <niravkumar.shah-c@zurichna.com>
2019-04-01 11:33:50 -0500

    added datetime functions.. convert the script to a function now

--- importDataYahoo.py
+++ importDataYahoo.py
@@ -3,26 +3,45 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
+from datetime import datetime, timezone
+
 
 ## convert to function
+def convertTimeStamp(date):
+    dt = datetime.strptime(date, '%Y-%m-%d')
+    return int(dt.replace(tzinfo=timezone.utc).timestamp())
+
+
 ## inputs are symbol and date
 # output will be a list of 2 dataframes.. for put & call respectively
 
-optionsURL = 'https://finance.yahoo.com/quote/TSLA/options?p=TSLA'
-optionsPage = urlopen(optionsURL)
-htmldata = BeautifulSoup(optionsPage)
+def getOptionsData(ticker = 'AAPL' , expDate = None):
+    optionsURL = 'https://finance.yahoo.com/quote/' + ticker + '/options?p=' + ticker
 
-## trying to fetch the html data
-temp = htmldata.find("table")
+    # if date is none.. return the most recent options data.. else filter for date
+    if expDate != None:
+        expDate  = convertTimeStamp(expDate)
+        optionsURL = optionsURL + '&date=' + str(expDate)
 
-headings = [th.get_text() for th in temp.find("tr").find_all("th")]
 
-datasets = pd.DataFrame()
-for row in temp.find_all("tr")[1:]:
-    dataset =pd.Series(td.get_text() for td in row.find_all("td"))
-    print(dataset)
-    datasets = datasets.append(dataset,ignore_index= True)
+    optionsPage = urlopen(optionsURL)
+    htmldata = BeautifulSoup(optionsPage)
 
-datasets.columns = ['ContractName','LastTradeDate','Strike','LastPrice','Bid','Ask','Change','PctChange',
-                   'Volume'	,'OpenInterest','ImpliedVolatility']
+    ## Trying to fetch the html data
+    temp = htmldata.find("table")
+
+    headings = [th.get_text() for th in temp.find("tr").find_all("th")]
+
+    datasets = pd.DataFrame()
+    for row in temp.find_all("tr")[1:]:
+        dataset = pd.Series(td.get_text() for td in row.find_all("td"))
+        datasets = datasets.append(dataset, ignore_index=True)
+
+    datasets.columns = ['ContractName', 'LastTradeDate', 'Strike', 'LastPrice', 'Bid', 'Ask', 'Change', 'PctChange',
+                        'Volume', 'OpenInterest', 'ImpliedVolatility']
+    return datasets
+
+
+
+
 
